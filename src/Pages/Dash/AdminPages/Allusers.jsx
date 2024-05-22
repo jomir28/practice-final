cbde16f921574c89b8e7e554d3bedc4f37a6a76c
unknown <jomirhossain15@gmail.com>
2024-05-23 01:50:38 +0600

    all users data loaded

--- src/Pages/Dash/AdminPages/Allusers.jsx
+++ src/Pages/Dash/AdminPages/Allusers.jsx
@@ -1,10 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../components/Hooks/useAxiosSecure";
 
 
 const Allusers = () => {
+
+    const axiosSecure = useAxiosSecure()
+
+    const {data: users} = useQuery({
+        queryKey: ['users'],
+        queryFn: async ()=> {
+            const res = await axiosSecure.get("/users")
+            return res.data;
+        }
+    })
+
     return (
         <div>
             <div>
-                <h2 className="text-2xl font-bold">Total User:</h2>
+                <h2 className="text-2xl font-bold">Total User:{users?.length}</h2>
             </div>
         </div>
     );
